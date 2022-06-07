0cf0cdfba61c75cefc42e8620a3019194e957d40
Srabonti Sen Neeti <105552240+SrabontiSenNeeti@users.noreply.github.com>
2022-06-07 21:50:10 +0600

    Create indention.py

---
+++ indention.py
@@ -0,0 +1,27 @@
+
+if 5>2:
+    print("Five is greater than 2")
+    
+    
+                                            
+if 2>5:
+    print("2 is less than 5")  
+    
+    
+if 5 > 22:
+    print("22 is greater than 5")
+else:
+    print("5 is less than 22")      
+ 
+ 
+    
+if 51<5:
+    print("False,Statement Skipped") 
+elif 0<5:
+    print("true,block Executed")
+elif 0<3:
+    print("true,block will not execute")
+else:
+    print("If all Fails") 
+    
+    
